2b70eb37c45fbbb2bd6c8281c379e62249afaa89
Job Feldbrugge <14946916+jfeldbrugge@users.noreply.github.com>
2026-05-08 00:51:40 -0400

    first commit

--- src/Nbody2D.jl
+++ src/Nbody2D.jl
@@ -1,5 +1,10 @@
+"""
+    A simple two-dimensional N-body code.
+"""
 module Nbody2D
+    using QuadGK, FFTW, Random, SpecialFunctions, ProgressMeter
 
-# Write your package code here.
+    include("Nbody_2D.jl")
 
+    export Box, Cosmology, GRF, zeldovich, LeapFrog
 end
---
+++ src/Nbody_2D.jl
@@ -0,0 +1,163 @@
+# Box
+# "Range in frequency space."
+# kRange(n, L) = fftfreq(n) * n * 2. * π / L
+
+struct Box
+    N 
+    L 
+    res
+    dim 
+    shape 
+    kx 
+    ky 
+
+    # Box(dim, N, L) = new(N, L, L / N, dim, fill(N, dim), 
+    # repeat(kRange(N, L), 1, N), 
+    # repeat(kRange(N, L), 1, N)')
+    Box(dim, N, L) = new(N, L, L / N, dim, fill(N, dim), 
+    repeat(fftfreq(N) * N * 2. * π / L, 1, N), 
+    repeat(fftfreq(N) * N * 2. * π / L, 1, N)')
+end
+
+# Cosmology
+struct Cosmology
+    H0
+    OmegaM
+    OmegaL
+    OmegaK
+    G
+    factor
+
+    Cosmology(H0, OmegaM, OmegaL) = 
+    new(H0, 
+        OmegaM, 
+        OmegaL, 
+        1. - OmegaM - OmegaL, 
+        3. / 2. * OmegaM * H0^2., 
+        growing_mode_norm(H0, OmegaM, OmegaL))
+end
+
+adot(a, cosmology::Cosmology) = cosmology.H0 * a * sqrt(cosmology.OmegaL + cosmology.OmegaM * a^-3. + cosmology.OmegaK * a^-2.)
+
+function growing_mode(a::AbstractFloat, cosmology::Cosmology)
+    if a <= 0.001
+        return a 
+    else
+        return cosmology.factor * adot(a, cosmology) / a * (quadgk(b -> adot(b, cosmology)^-3., 0.00001, a, rtol=1e-6)[1])
+    end
+end
+
+function growing_mode(as::Vector, cosmology::Cosmology)
+    return [growing_mode(a, cosmology) for a in as]
+end
+
+function growing_mode_norm(H0, OmegaM, OmegaL)
+    adot(a) = H0 * a * sqrt(OmegaL + OmegaM * a^-3. + (1. - OmegaM - OmegaL) * a^-2.)
+    return 1. / (adot(1.) * quadgk(b -> adot(b)^-3., 0.00001, 1., rtol=1e-6)[1])
+end
+
+# Gaussian random field
+"Generate an unconstrained Gaussian random field."
+function GRF(ns, Rs, α, seed, box::Box)
+    Random.seed!(seed)
+
+    "Power spectrum."
+    function P(k, ns, Rs, α)
+        return α^2 * 4. * π * Rs^(2. + ns) / gamma(1. + ns / 2.) * k^(ns - 4.) * exp(-Rs^2 * k^2)
+    end
+
+    Pk = P.(sqrt.(box.kx.^2 + box.ky.^2), ns, Rs, α)
+    Pk[1, 1] = 0.
+    return real(ifft(sqrt.(Pk) .* fft(randn(box.N, box.N)))) * box.N^(2/2) / box.L^(2/2)
+end
+
+# Utility functions
+function Grad2(data, i) 
+    if i == 1
+        return 1. / 12. .* circshift(data, (2,0)) - 2. / 3. .* circshift(data, (1,0)) + 2. / 3. .* circshift(data, (-1,0)) -  1. / 12. .* circshift(data, (-2,0)) 
+    else i == 2
+        return 1. / 12. .* circshift(data, (0,2)) - 2. / 3. .* circshift(data, (0,1)) + 2. / 3. .* circshift(data, (0,-1)) -  1. / 12. .* circshift(data, (0,-2)) 
+    end
+end
+
+function Interp2D(data, x)
+    N = size(data, 1)
+    X1 = mod.(floor.(Int, x), N) .+ 1
+    X2 = mod.(ceil.(Int, x),  N) .+ 1
+    xm = mod.(x, 1.)
+    xn = 1. .- xm
+
+    f1 = [data[X1[i,1], X1[i,2]] for i in 1:size(X1,1)]
+    f2 = [data[X2[i,1], X1[i,2]] for i in 1:size(X1,1)]
+    f3 = [data[X1[i,1], X2[i,2]] for i in 1:size(X1,1)]
+    f4 = [data[X2[i,1], X2[i,2]] for i in 1:size(X1,1)]
+
+    return f1 .* xn[:,1] .* xn[:,2] + f2 .* xm[:,1] .* xn[:,2] + f3 .* xn[:,1] .* xm[:,2] + f4 .* xm[:,1] .* xm[:,2] 
+end
+
+function CIC(X, box::Box)
+    delta = zeros(box.N, box.N)
+    pos = mod.(hcat(vec(X[:,:,1]'), vec(X[:,:,2]')), box.L) ./ box.res
+    for i in 1:size(pos, 1)
+        idx1, idx2 = floor(Int, pos[i, 1]), floor(Int, pos[i, 2])
+        f1, f2     = pos[i, 1] - idx1, pos[i, 2] - idx2
+        delta[mod1(idx1 + 1, box.N), mod1(idx2 + 1, box.N)] += (1. - f1) * (1. - f2) 
+        delta[mod1(idx1 + 2, box.N), mod1(idx2 + 1, box.N)] += f1 * (1. - f2) 
+        delta[mod1(idx1 + 1, box.N), mod1(idx2 + 2, box.N)] += (1. - f1) * f2
+        delta[mod1(idx1 + 2, box.N), mod1(idx2 + 2, box.N)] += f1 * f2
+    end
+    return delta
+end
+
+# Nbody
+mutable struct State 
+    time 
+    position
+    momentum
+end
+
+"Evaluate the Zel'dovich approximation."
+function zeldovich(phi, a_pos, a_vel, box::Box, cosmology::Cosmology)
+    u_x, u_y = -box.N / box.L .* Grad2(phi, 1), -box.N / box.L .* Grad2(phi, 2)
+
+    qRange = range(0. , box.L, box.N + 1)[2:end]
+    q_x = repeat(qRange, 1, box.N)
+    q_y = q_x'
+
+    Dp_pos = growing_mode(a_pos, cosmology)
+    Dp_vel = growing_mode(a_vel, cosmology)
+    X = stack((q_x .+ Dp_pos * u_x, q_y .+ Dp_pos * u_y))
+    P = stack((Dp_vel * u_x, Dp_vel * u_y))
+    
+    return State(a_pos, X, P)
+end
+
+function Drift(a, δa, P, cosmology::Cosmology) 
+    return δa .* P / (a^2 * adot(a, cosmology))
+end
+
+function Kick(a, δa, X, box::Box, m, cosmology::Cosmology)
+    delta = CIC(X, box) .* m .- 1.
+    phi_f = fft(delta) ./ (box.kx.^2 .+ box.ky.^2)
+    phi_f[1,1] = 0.
+    phi = real(ifft(phi_f)) .* cosmology.G ./ a
+
+    u_x, u_y = box.N / box.L .* Grad2(phi, 1), box.N / box.L .* Grad2(phi, 2)
+    
+    acc_x = Interp2D(u_x, mod.(hcat(vec(X[:,:,1]'), vec(X[:,:,2]')), box.L) ./ box.res)
+    acc_y = Interp2D(u_y, mod.(hcat(vec(X[:,:,1]'), vec(X[:,:,2]')), box.L) ./ box.res)
+    acc = -stack((reshape(acc_x, (box.N ÷ 2, box.N ÷ 2))', reshape(acc_y, (box.N ÷ 2, box.N ÷ 2))'))
+    return δa / adot(a, cosmology) .* acc
+end
+
+function LeapFrog(phi, ai, af, δa, box::Box, cosmology::Cosmology)
+    force_box = Box(box.dim, 2 * box.N, box.L)
+    mass = (force_box.N / box.N)^box.dim
+    state = zeldovich(phi, ai, ai + δa / 2., box, cosmology)
+    @showprogress for a in ai:δa:af
+        # println("a = ", a)
+        state.position += Drift(a, δa, state.momentum, cosmology)
+        state.momentum -= Kick(a + δa / 2., δa, state.position, force_box, mass, cosmology)
+    end
+    return state
+end
