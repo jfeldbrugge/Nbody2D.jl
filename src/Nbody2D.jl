ac6e5c21d62fe36d4c9e1785bf6babd445d51385
Job Feldbrugge <14946916+jfeldbrugge@users.noreply.github.com>
2026-05-09 07:26:39 -0400

    update

--- src/Nbody2D.jl
+++ src/Nbody2D.jl
@@ -4,7 +4,6 @@
 module Nbody2D
     using QuadGK, FFTW, Random, SpecialFunctions
     using LinearAlgebra, StaticArrays, DelaunayTriangulation
-    # , Delaunay
 
     include("Nbody.jl")
     include("PhaseSpaceDTFE.jl")
