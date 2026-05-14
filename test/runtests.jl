ccc001963f20497fddb08c4d02f422ba34f5a5e1
Job Feldbrugge <14946916+jfeldbrugge@users.noreply.github.com>
2026-05-14 13:05:58 +0100

    Add to runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,6 +22,7 @@ using Test
     stateZeldovich = Zeldovich(phi, af, af, box, EdS)
     stateNbody = LeapFrog(phi, ai, af, δa, box, EdS)
 
+    # Phase-Space Delaunay Tesselation Field Estimator
     let depth = 5
         stateInitial = Zeldovich(phi, δa, δa, box, EdS)
         global estimatorZeldovich = PS_DTFE(stateInitial, stateZeldovich, depth, box)
@@ -36,6 +37,7 @@ using Test
     # @test ps_velocity(p, estimatorZeldovich) ≈ [-1.759013770242557 -0.09413530535089731]
     @test numberOfStreams(p, estimatorZeldovich) == 1
 
+    # Constrained Gaussian Random Field
     let ns = 2., Rs = 1., α = 1., seed = 1, σ = 1., ders = [0 0; 1 0; 0 1]
         global cgrf = cGRF(box, k -> P(k, ns, Rs, α), σ, ders)
         c = [1, 0, 0]
@@ -43,9 +45,19 @@ using Test
         global f_var = varianceField(cgrf)
     end
     mc = measureConstraints(f_c, cgrf)
-    @test mc[1] ≈ 1
+    @test abs(mc[1] - 1) < 10e-4
+    @test abs(mc[2]) < 10e-4
+    @test abs(mc[3]) < 10e-4
 
+    gra = gradient(f, box)
+    hes = hessian(f, box)
+    lap = laplacian(f, box)
+    smo = smooth(f, 1., box)
 
+    @test gra[box.N ÷ 2, box.N ÷ 2, 1] ≈ -1.3114975735196817
+    @test hes[box.N ÷ 2, box.N ÷ 2 , 1] ≈ 0.3349705154191447
+    @test lap[box.N ÷ 2, box.N ÷ 2] ≈ 1.1604751729595333
+    @test smo[box.N ÷ 2, box.N ÷ 2] ≈ -2.060513408055121
 
     println("Finish test.")
 end
